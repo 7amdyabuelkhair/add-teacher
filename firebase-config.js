68bdd0f0a6db99ccbd2e863f7d9945952279898e
Hamdy Abu El-Khair <hamdyabuelkhair@gmail.com>
2026-02-23 06:44:37 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration (used by add-trainer.html, trainers.html, dashboard.html)
+window.STEMIFY_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC_LWLJZp3MErC_KTCLqMQir6R7zavlKEk",
+  authDomain: "add-teacher-5324e.firebaseapp.com",
+  projectId: "add-teacher-5324e",
+  storageBucket: "add-teacher-5324e.firebasestorage.app",
+  messagingSenderId: "54128608930",
+  appId: "1:54128608930:web:f8d8f851e230807fbab9fb",
+  measurementId: "G-NE6RZ3119W"
+};
